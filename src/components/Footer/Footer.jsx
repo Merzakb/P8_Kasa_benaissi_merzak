352acf04372beed4174e276f2864cbadb8ebe158
Merzak Benaissi <merzak89merzak@gmail.com>
2023-12-22 23:45:19 +0100

    create Header & Footer components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,17 @@
 
-
+import { Link } from "react-router-dom"
+import Logo from "../../assets/logo-footer.png"
 
 function Footer() {
     return (
-        <div >
-            <p>Footer</p>
-        </div>
+        <footer>
+            <div className="footer">
+                <Link to="/">
+                    <img src={Logo}  alt="logo kasa" className="footer__logo" />
+                </Link>
+                <p className="footer__right">© 2020 Kasa. All rights reserved</p>
+            </div>
+        </footer>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom"
+import Logo from "../../assets/logo-header.svg"
+
 function Header() {
     return (
-        <div >
-            <p>Header</p>
-        </div>
+        <header className="header">
+            <Link to="/">
+                <img src={Logo}  alt="logo kasa" className="header__logo" />
+            </Link>
+            <nav className="header__navbar">
+                <Link to="/" className="header__navbar--item">Acceuil</Link>
+                <Link to="/about" className="header__navbar--item">A Propos</Link>
+            </nav>
+        </header>
   );
 }
 
