c5943459d156c38332dec499782935f6653f3bfe
Merzak Benaissi <merzak89merzak@gmail.com>
2023-12-22 23:50:50 +0100

    create Header & Footer components

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/LOGO.svg"
+
+function Banner() {
+    return (
+        <div>
+            
+        </div>
+  )
+}
+
+export default Banner;
